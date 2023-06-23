e6a5a971e5a19194c8c5011b3a86d7d08fa2d8ad
Martin Matovu <matovius@proton.me>
2023-06-24 00:39:12 +0300

    Change text font color to inherit

--- src/components/typography/Text.svelte
+++ src/components/typography/Text.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  export let alignment: "left" | "center" | "right" | "justify" | "inherit" =
-    "inherit";
   export let as:
     | "h1"
     | "h2"
@@ -11,46 +9,42 @@
     | "p"
     | "span"
     | "legend";
-  export let breakWord: boolean = false;
-  export let color: "gray" | "sky" = "gray";
-  export let fontWeight: "regular" | "medium" | "semibold" | "bold" | null =
-    null;
 </script>
 
 {#if as === "h1"}
-  <h1 class="font-bold text-5xl">
+  <h1 class="font-bold text-5xl text-inherit">
     <slot />
   </h1>
 {:else if as === "h2"}
-  <h2 class="font-bold text-4xl">
+  <h2 class="font-bold text-4xl text-inherit">
     <slot />
   </h2>
 {:else if as === "h3"}
-  <h3 class="font-semibold text-3xl">
+  <h3 class="font-semibold text-3xl text-inherit">
     <slot />
   </h3>
 {:else if as === "h4"}
-  <h4 class="font-semibold text-2xl">
+  <h4 class="font-semibold text-2xl text-inherit">
     <slot />
   </h4>
 {:else if as === "h5"}
-  <h5 class="font-bold text-xl">
+  <h5 class="font-bold text-xl text-inherit">
     <slot />
   </h5>
 {:else if as === "h6"}
-  <h6 class="font-bold text-lg">
+  <h6 class="font-bold text-lg text-inherit">
     <slot />
   </h6>
 {:else if as === "p"}
-  <p class="font-regular text-md">
+  <p class="font-regular text-md text-inherit">
     <slot />
   </p>
 {:else if as === "span"}
-  <span class="font-regular text-sm">
+  <span class="font-regular text-sm text-inherit">
     <slot />
   </span>
 {:else if as === "legend"}
-  <legend class="font-bold text-xs">
+  <legend class="font-bold text-xs text-inherit">
     <slot />
   </legend>
 {/if}
