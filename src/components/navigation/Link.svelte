7943665fe486eca7210addcbe392938162b56ebe
Martin Matovu <matovius@proton.me>
2023-06-24 00:36:40 +0300

    Implement basic landing page header

--- src/components/navigation/Link.svelte
+++ src/components/navigation/Link.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  export let route = "/";
+  export let route: string;
+  export let label: string;
 </script>
 
 <a
   href={route}
   class="transition font-medium text-base text-sky-500 underline decoration-sky-100 decoration-2 hover:decoration-sky-300"
 >
-  <slot />
+  {label}
 </a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import Link from "../components/navigation/Link.svelte";
+  import Text from "../components/typography/Text.svelte";
+
+  interface NavLink {
+    name: string;
+    route: string;
+  }
+  const navItems: NavLink[] = [
+    {
+      name: "Features",
+      route: "#features",
+    },
+    {
+      name: "Pricing",
+      route: "#pricing",
+    },
+  ];
+</script>
+
+<header
+  class="
+    w-full
+    border-b-2
+    border-slate-300
+    bg-slate-100
+    flex
+    justify-between
+    items-center"
+>
+  <div class="p-2">
+    <div
+      class="w-8 aspect-square border-2 border-slate-300 bg-slate-200 rounded-lg"
+    />
+  </div>
+  <div class="p-2 flex justify-center items-center gap-2">
+    <nav class="flex justify-center items-center">
+      <ul class="flex justify-center items-center gap-2">
+        {#each navItems as navItem}
+          <li>
+            <Link label={navItem.name} route={navItem.route} />
+          </li>
+        {/each}
+      </ul>
+    </nav>
+    <a
+      href="/demo"
+      class="transition px-2 py-1 border-2 flex justify-center items-center gap-2 no-underline border-slate-300 rounded-lg bg-slate-100 hover:border-slate-500 hover:bg-slate-500"
+    >
+      <Text as="span">Start Demo</Text>
+    </a>
+  </div>
+</header>
+<main />
+<footer />
