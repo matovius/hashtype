14bb81d0853f820b5b14dc20b718fe3015bcd582
Martin Matovu <matovius@proton.me>
2023-06-24 13:56:27 +0300

    Adjust page responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
     bg-slate-100
     flex
     justify-center
-    items-center fixed top-0 z-10"
+    items-center sticky top-0 z-10"
 >
   <div class="w-full max-w-5xl flex justify-between items-center">
     <div>
@@ -54,13 +54,15 @@
     </div>
   </div>
 </header>
-<main class="w-full bg-slate-50 flex flex-col justify-center scroll-smooth">
+<main class="w-full bg-slate-50 flex flex-col justify-center">
   <section
     id="hero-section"
-    class="text-slate-100 w-full min-h-screen px-6 pt-[104px] pb-6 bg-slate-800 flex flex-col justify-start items-center gap-10 relative"
+    class="text-slate-100 w-full h-full p-6 bg-slate-800 flex flex-col justify-start items-center gap-10 relative"
   >
     <div class="w-full max-w-5xl flex flex-col items-center gap-8 pt-20">
-      <div class="w-full max-w-lg flex flex-col items-center gap-8 text-center">
+      <div
+        class="w-full min-w-[320px] max-w-lg flex flex-col items-center gap-8 text-center"
+      >
         <div class="flex flex-col gap-4">
           <Text as="h1">The Minimal Markdown Text Editor</Text>
           <Text as="p"
@@ -76,42 +78,20 @@
         </a>
       </div>
       <div
-        class="w-full max-w-lg aspect-video bg-slate-700 rounded-xl"
+        class="w-full min-w-[320px] max-w-lg aspect-video bg-slate-700 rounded-xl"
         title="Hero image goes here"
       />
     </div>
     <div
-      class="p-2 rounded-lg absolute bottom-6 border-2 border-slate-500 bg-slate-500/50 flex justify-center items-center gap-2"
+      class="p-2 rounded-lg absolute bottom-6 right-6 border-2 border-slate-500 bg-slate-500/50 flex justify-center items-center gap-2"
     >
       <Text as="p">Scroll down</Text>
     </div>
   </section>
   <section id="features" class="text-slate-900 w-full min-h-screen p-6">
-    <div class="w-full max-w-5xl pt-16">
+    <div class="w-full max-w-5xl">
       <Text as="h2">Features</Text>
     </div>
   </section>
-  <div class="w-full max-w-5xl flex flex-col justify-center items-center">
-    <div
-      class="min-h-[400px] flex flex-col justify-center items-center gap-12 bg-slate-800 rounded-3xl p-12 text-slate-100 relative"
-    >
-      <div class="w-full h-full flex flex-col gap-8">
-        <Text as="h1">A minimal experience for greater productivity!</Text>
-        <Text as="p"
-          >Have fewer distractions in this minimalist-style text editor.</Text
-        >
-        <a
-          href="/demo"
-          class="transition w-full md:w-fit px-8 py-4 flex justify-center items-center gap-2 no-underline text-sky-100 rounded-lg bg-sky-500 shadow-md shadow-sky-900 hover:shadow-lg hover:shadow-sky-700 hover:bg-sky-400"
-        >
-          <Text as="span">Start Demo</Text>
-        </a>
-      </div>
-      <div
-        class="w-full aspect-video bg-slate-700 rounded-xl"
-        title="Hero image goes here"
-      />
-    </div>
-  </div>
 </main>
 <footer />
