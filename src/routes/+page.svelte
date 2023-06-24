aad9ac690af08bde9b6694675d3815108ed81706
Martin Matovu <matovius@proton.me>
2023-06-24 17:29:57 +0300

    Wrap landing page in a div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,139 +38,145 @@
   ];
 </script>
 
-<Header />
-<main class="w-full bg-slate-50 flex flex-col justify-center">
-  <section
-    id="hero-section"
-    class="text-slate-100 w-full h-full p-6 bg-slate-800 flex flex-col justify-start items-center gap-10 relative"
-  >
-    <div class="w-full max-w-5xl flex flex-col items-center gap-8 pt-20">
-      <div
-        class="w-full min-w-[320px] max-w-lg flex flex-col items-center gap-8 text-center"
-      >
-        <div class="flex flex-col gap-4">
-          <Text as="h1">The Minimal Markdown Text Editor</Text>
-          <Text as="p"
-            >Have fewer distractions with this ultra-minimalist text editor for
-            greater productivity
-          </Text>
-        </div>
-        <a
-          href="/demo"
-          class="transition w-full sm:w-fit px-8 py-4 flex justify-center items-center gap-2 no-underline text-sky-100 rounded-lg bg-sky-500 shadow-md shadow-sky-900 hover:shadow-lg hover:shadow-sky-700 hover:bg-sky-400"
-        >
-          <Text as="span">Start Demo</Text>
-        </a>
-      </div>
-      <div
-        class="w-full min-w-[320px] max-w-lg aspect-video bg-slate-700 rounded-xl"
-        title="Hero image goes here"
-      />
-    </div>
-  </section>
-  <section
-    id="features"
-    class="text-slate-900 w-full px-6 py-24 flex justify-center items-center"
-  >
-    <div class="w-full h-full max-w-5xl">
-      <div class="text-center">
-        <Text as="h2">Features</Text>
-      </div>
-
-      <div class="w-full flex gap-6 flex-wrap py-6 justify-center items-center">
-        {#each features as feature}
-          <div
-            class="transition delay-200 w-80 h-40 p-4 rounded-2xl border-2 bg-slate-100 border-slate-300 hover:scale-125 hover:shadow-lg hover: shadow-slate-300"
-          >
-            <Text as="h5">{feature.name}</Text>
-            <Text as="p">{feature.description}</Text>
-          </div>
-        {/each}
-      </div>
-    </div>
-  </section>
-  <section
-    id="pricing"
-    class="text-slate-900 w-full px-6 py-24 flex justify-center items-center"
-  >
-    <div class="w-full h-full max-w-5xl">
-      <div class="text-center">
-        <Text as="h2">Pricing</Text>
-      </div>
-
-      <div class="w-full flex gap-6 flex-wrap py-6 justify-center items-center">
+<div class="w-full h-full">
+  <Header />
+  <main class="w-full bg-slate-50 flex flex-col justify-center">
+    <section
+      id="hero-section"
+      class="text-slate-100 w-full h-full p-6 bg-slate-800 flex flex-col justify-start items-center gap-10 relative"
+    >
+      <div class="w-full max-w-5xl flex flex-col items-center gap-8 pt-20">
         <div
-          id="basic-card"
-          class="transition delay-200 w-80 h-80 rounded-2xl flex flex-col border-2 border-slate-300 bg-slate-100 overflow-hidden hover:scale-125 hover:z-10"
+          class="w-full min-w-[320px] max-w-lg flex flex-col items-center gap-8 text-center"
         >
-          <div
-            class="p-4 border-b-2 border-slate-300 flex justify-center items-center"
-          >
-            <Text as="h5">Basic</Text>
+          <div class="flex flex-col gap-4">
+            <Text as="h1">The Minimal Markdown Text Editor</Text>
+            <Text as="p"
+              >Have fewer distractions with this ultra-minimalist text editor
+              for greater productivity
+            </Text>
           </div>
-          <div
-            class="p-4 h-full flex flex-col justify-center items-center gap-4"
+          <a
+            href="/demo"
+            class="transition w-full sm:w-fit px-8 py-4 flex justify-center items-center gap-2 no-underline text-sky-100 rounded-lg bg-sky-500 shadow-md shadow-sky-900 hover:shadow-lg hover:shadow-sky-700 hover:bg-sky-400"
           >
-            <span class="text-6xl font-bold">FREE</span>
-            <span class="text-sm font-semibold text-slate-500"
-              >No account creation needed</span
-            >
-          </div>
-          <div
-            class="p-4 border-t-2 border-slate-300 flex justify-center items-center"
-          >
-            <a
-              href="/demo"
-              class="transition w-full px-8 py-4 flex justify-center items-center gap-2 no-underline text-sky-100 rounded-lg bg-sky-500 hover:bg-sky-600"
-            >
-              <Text as="span">Start Demo</Text>
-            </a>
-          </div>
+            <Text as="span">Start Demo</Text>
+          </a>
         </div>
         <div
-          id="premium-card"
-          class="transition w-80 h-80 rounded-2xl flex flex-col border-2 border-slate-300 bg-slate-100 overflow-hidden relative"
+          class="w-full min-w-[320px] max-w-lg aspect-video bg-slate-700 rounded-xl"
+          title="Hero image goes here"
+        />
+      </div>
+    </section>
+    <section
+      id="features"
+      class="text-slate-900 w-full px-6 py-24 flex justify-center items-center"
+    >
+      <div class="w-full h-full max-w-5xl">
+        <div class="text-center">
+          <Text as="h2">Features</Text>
+        </div>
+
+        <div
+          class="w-full flex gap-6 flex-wrap py-6 justify-center items-center"
         >
-          <div
-            class="p-4 border-b-2 text-sky-100 border-slate-300 bg-sky-500 flex justify-center items-center"
-          >
-            <Text as="h5">Premium</Text>
-          </div>
-          <div
-            class="p-4 h-full flex flex-col justify-center items-center gap-4"
-          >
-            <span class="text-6xl font-bold">SOON</span>
-            <div class="flex flex-col">
-              <span class="text-sm font-semibold text-slate-500"
-                >Sync across all your devices</span
-              >
-              <span class="text-sm font-semibold text-slate-500"
-                >Accont creation needed</span
-              >
+          {#each features as feature}
+            <div
+              class="transition delay-200 w-80 h-40 p-4 rounded-2xl border-2 bg-slate-100 border-slate-300 hover:scale-125 hover:shadow-lg hover: shadow-slate-300"
+            >
+              <Text as="h5">{feature.name}</Text>
+              <Text as="p">{feature.description}</Text>
             </div>
-          </div>
+          {/each}
+        </div>
+      </div>
+    </section>
+    <section
+      id="pricing"
+      class="text-slate-900 w-full px-6 py-24 flex justify-center items-center"
+    >
+      <div class="w-full h-full max-w-5xl">
+        <div class="text-center">
+          <Text as="h2">Pricing</Text>
+        </div>
+
+        <div
+          class="w-full flex gap-6 flex-wrap py-6 justify-center items-center"
+        >
           <div
-            class="p-4 border-t-2 border-slate-300 flex justify-center items-center"
-          >
-            <a
-              href="/"
-              class="transition w-full px-8 py-4 flex justify-center items-center gap-2 no-underline text-slate-100 rounded-lg bg-slate-500 hover:bg-slate-600"
-            >
-              <Text as="span">Coming Soon</Text>
-            </a>
-          </div>
-          <div
-            class="absolute inset-0 bg-slate-100/95 flex justify-center items-center"
+            id="basic-card"
+            class="transition delay-200 w-80 h-80 rounded-2xl flex flex-col border-2 border-slate-300 bg-slate-100 overflow-hidden hover:scale-125 hover:z-10"
           >
             <div
-              class="px-4 py-2 rounded-lg bg-slate-500 border-2 border-slate-300 text-slate-100 flex justify-center items-center"
+              class="p-4 border-b-2 border-slate-300 flex justify-center items-center"
             >
-              <Text as="span">Coming Soon</Text>
+              <Text as="h5">Basic</Text>
+            </div>
+            <div
+              class="p-4 h-full flex flex-col justify-center items-center gap-4"
+            >
+              <span class="text-6xl font-bold">FREE</span>
+              <span class="text-sm font-semibold text-slate-500"
+                >No account creation needed</span
+              >
+            </div>
+            <div
+              class="p-4 border-t-2 border-slate-300 flex justify-center items-center"
+            >
+              <a
+                href="/demo"
+                class="transition w-full px-8 py-4 flex justify-center items-center gap-2 no-underline text-sky-100 rounded-lg bg-sky-500 hover:bg-sky-600"
+              >
+                <Text as="span">Start Demo</Text>
+              </a>
+            </div>
+          </div>
+          <div
+            id="premium-card"
+            class="transition w-80 h-80 rounded-2xl flex flex-col border-2 border-slate-300 bg-slate-100 overflow-hidden relative"
+          >
+            <div
+              class="p-4 border-b-2 text-sky-100 border-slate-300 bg-sky-500 flex justify-center items-center"
+            >
+              <Text as="h5">Premium</Text>
+            </div>
+            <div
+              class="p-4 h-full flex flex-col justify-center items-center gap-4"
+            >
+              <span class="text-6xl font-bold">SOON</span>
+              <div class="flex flex-col">
+                <span class="text-sm font-semibold text-slate-500"
+                  >Sync across all your devices</span
+                >
+                <span class="text-sm font-semibold text-slate-500"
+                  >Accont creation needed</span
+                >
+              </div>
+            </div>
+            <div
+              class="p-4 border-t-2 border-slate-300 flex justify-center items-center"
+            >
+              <a
+                href="/"
+                class="transition w-full px-8 py-4 flex justify-center items-center gap-2 no-underline text-slate-100 rounded-lg bg-slate-500 hover:bg-slate-600"
+              >
+                <Text as="span">Coming Soon</Text>
+              </a>
+            </div>
+            <div
+              class="absolute inset-0 bg-slate-100/95 flex justify-center items-center"
+            >
+              <div
+                class="px-4 py-2 rounded-lg bg-slate-500 border-2 border-slate-300 text-slate-100 flex justify-center items-center"
+              >
+                <Text as="span">Coming Soon</Text>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-</main>
-<footer class="w-full bg-slate-800 text-slate-100" />
+    </section>
+  </main>
+  <footer class="w-full bg-slate-800 text-slate-100" />
+</div>
