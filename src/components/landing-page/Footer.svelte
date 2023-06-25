4c826e164d609bb8a4464224ec9e3da67eb5569c
Martin Matovu <matovius@proton.me>
2023-06-25 11:44:02 +0300

    Extract footer into its own component

---
+++ src/components/landing-page/Footer.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+  import SunIcon from "../icons/SunIcon.svelte";
+
+  import Text from "../typography/Text.svelte";
+
+  interface AppLink {
+    label: string;
+    route: string;
+  }
+
+  interface ExternalLink {
+    label: string;
+    link: string;
+  }
+
+  const appLinks: AppLink[] = [
+    {
+      label: "Releases",
+      route: "/releases",
+    },
+    {
+      label: "About",
+      route: "/about",
+    },
+    {
+      label: "Contact",
+      route: "/contact",
+    },
+    {
+      label: "Legal",
+      route: "/legal",
+    },
+  ];
+
+  const externalLinks: ExternalLink[] = [
+    {
+      label: "GitHub",
+      link: "https://github.com/matovius",
+    },
+    {
+      label: "Mastodon",
+      link: "https://mastodon.social/@matovius",
+    },
+    {
+      label: "Twitter",
+      link: "https://twitter.com/@matovius_",
+    },
+    {
+      label: "LinkedIn",
+      link: "https://linkedin.com/", //TODO: Complete the link here
+    },
+  ];
+</script>
+
+<footer
+  class="w-full bg-slate-800 text-slate-100 flex flex-col justify-center items-center"
+>
+  <div class="w-full max-w-5xl px-4 py-10 flex flex-col gap-12 sm:flex-row">
+    <div
+      class="w-full flex flex-col justify-between sm:justify-center items-center gap-6 sm:gap-2"
+    >
+      <div
+        class="w-12 aspect-square rounded-lg border-2 border-slate-300 bg-slate-200"
+        title="App icon goes here"
+      />
+      <button
+        class="transition w-full max-w-sm p-4 rounded-lg border-2 border-slate-500 hover:border-slate-300 flex justify-between items-center bg-slate-700 hover:bg-slate-600"
+      >
+        <Text as="span">Theme</Text>
+        <SunIcon />
+      </button>
+    </div>
+    <div class="w-full flex justify-between align-center">
+      <div class="w-full">
+        <ul
+          class="list-none p-0 m-0 flex flex-col justify-center items-center gap-4"
+        >
+          {#each appLinks as appLink}
+            <li>
+              <a
+                href={appLink.route}
+                class="transition font-bold underline underline-offset-2 decoration-2 decoration-slate-500 hover:decoration-slate-300"
+              >
+                <Text as="p">{appLink.label}</Text>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <div class="w-full">
+        <ul
+          class="list-none p-0 m-0 flex flex-col justify-center items-center gap-4"
+        >
+          {#each externalLinks as externalLink}
+            <li>
+              <a
+                href={externalLink.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                class="transition font-bold underline underline-offset-2 decoration-2 decoration-slate-500 hover:decoration-slate-300"
+              >
+                <Text as="p">{externalLink.label}</Text>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div
+    class="w-full flex justify-center items-center border-t-2 border-slate-500"
+  >
+    <div
+      class="w-full max-w-5xl p-4 flex flex-col gap-2 sm:flex-row sm:justify-between"
+    >
+      <Text as="span">Copyright 2023, Hashtype. All Rights Reserved</Text>
+      <Text as="span">Made with passion, and SvelteKit</Text>
+    </div>
+  </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import SunIcon from "../components/icons/SunIcon.svelte";
   import Header from "../components/landing-page/Header.svelte";
+  import Footer from "../components/landing-page/Footer.svelte";
 
   import Link from "../components/navigation/Link.svelte";
   import Text from "../components/typography/Text.svelte";
@@ -227,70 +227,5 @@
       </div>
     </section>
   </main>
-  <footer
-    class="w-full bg-slate-800 text-slate-100 flex flex-col justify-center items-center"
-  >
-    <div class="w-full max-w-5xl px-4 py-10 flex flex-col gap-12 sm:flex-row">
-      <div
-        class="w-full flex flex-col justify-between sm:justify-center items-center gap-6 sm:gap-2"
-      >
-        <div
-          class="w-12 aspect-square rounded-lg border-2 border-slate-300 bg-slate-200"
-          title="App icon goes here"
-        />
-        <button
-          class="transition w-full max-w-sm p-4 rounded-lg border-2 border-slate-500 hover:border-slate-300 flex justify-between items-center bg-slate-700 hover:bg-slate-600"
-        >
-          <Text as="span">Theme</Text>
-          <SunIcon />
-        </button>
-      </div>
-      <div class="w-full flex justify-between align-center">
-        <div class="w-full">
-          <ul
-            class="list-none p-0 m-0 flex flex-col justify-center items-center gap-4"
-          >
-            {#each appLinks as appLink}
-              <li>
-                <a
-                  href={appLink.route}
-                  class="transition font-bold underline underline-offset-2 decoration-2 decoration-slate-500 hover:decoration-slate-300"
-                >
-                  <Text as="p">{appLink.label}</Text>
-                </a>
-              </li>
-            {/each}
-          </ul>
-        </div>
-        <div class="w-full">
-          <ul
-            class="list-none p-0 m-0 flex flex-col justify-center items-center gap-4"
-          >
-            {#each externalLinks as externalLink}
-              <li>
-                <a
-                  href={externalLink.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  class="transition font-bold underline underline-offset-2 decoration-2 decoration-slate-500 hover:decoration-slate-300"
-                >
-                  <Text as="p">{externalLink.label}</Text>
-                </a>
-              </li>
-            {/each}
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div
-      class="w-full flex justify-center items-center border-t-2 border-slate-500"
-    >
-      <div
-        class="w-full max-w-5xl p-4 flex flex-col gap-2 sm:flex-row sm:justify-between"
-      >
-        <Text as="span">Copyright 2023, Hashtype. All Rights Reserved</Text>
-        <Text as="span">Made with passion, and SvelteKit</Text>
-      </div>
-    </div>
-  </footer>
+  <Footer />
 </div>
