1e54c06f246f7db4885e9f1bf3646d7c43497539
Martin Matovu <matovius@proton.me>
2023-06-25 11:31:05 +0300

    Update font families

--- src/components/typography/Text.svelte
+++ src/components/typography/Text.svelte
@@ -12,39 +12,39 @@
 </script>
 
 {#if as === "h1"}
-  <h1 class="font-bold text-5xl text-inherit">
+  <h1 class="font-bold font-display text-5xl text-inherit">
     <slot />
   </h1>
 {:else if as === "h2"}
-  <h2 class="font-bold text-4xl text-inherit">
+  <h2 class="font-bold font-display text-4xl text-inherit">
     <slot />
   </h2>
 {:else if as === "h3"}
-  <h3 class="font-semibold text-3xl text-inherit">
+  <h3 class="font-semibold font-display text-3xl text-inherit">
     <slot />
   </h3>
 {:else if as === "h4"}
-  <h4 class="font-semibold text-2xl text-inherit">
+  <h4 class="font-semibold font-display text-2xl text-inherit">
     <slot />
   </h4>
 {:else if as === "h5"}
-  <h5 class="font-bold text-xl text-inherit">
+  <h5 class="font-bold font-display text-xl text-inherit">
     <slot />
   </h5>
 {:else if as === "h6"}
-  <h6 class="font-bold text-lg text-inherit">
+  <h6 class="font-bold font-display text-lg text-inherit">
     <slot />
   </h6>
 {:else if as === "p"}
-  <p class="font-regular text-md text-inherit">
+  <p class="font-regular font-body text-md text-inherit">
     <slot />
   </p>
 {:else if as === "span"}
-  <span class="font-bold text-sm text-inherit">
+  <span class="font-bold font-body text-sm text-inherit">
     <slot />
   </span>
 {:else if as === "legend"}
-  <legend class="font-bold text-xs text-inherit">
+  <legend class="font-bold font-body text-xs text-inherit">
     <slot />
   </legend>
 {/if}
