67c6c5e200f81c636c8c0e5b76644664f2ef3c6c
Martin Matovu <matovius@proton.me>
2023-06-26 02:26:52 +0300

    Implement better text colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,6 @@
   <link rel="icon" href={logo} type="image/png" />
 </svelte:head>
 
-<div class="bg-slate-50 dark:bg-slate-900">
+<div class="bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-100">
   <slot />
 </div>
