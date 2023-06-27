923a7ccd3e77679f099a29015e741a648d2d8b6e
Martin Matovu <matovius@proton.me>
2023-06-27 10:55:50 +0300

    Add theme switcher and nav tooltips

--- src/routes/demo/+layout.svelte
+++ src/routes/demo/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
   import Bars2Icon from "../../components/icons/Bars2Icon.svelte";
   import DocumentDuplicateIcon from "../../components/icons/DocumentDuplicateIcon.svelte";
   import FolderIcon from "../../components/icons/FolderIcon.svelte";
@@ -7,24 +8,66 @@
   import TrashIcon from "../../components/icons/TrashIcon.svelte";
   import SunIcon from "../../components/icons/SunIcon.svelte";
   import MoonIcon from "../../components/icons/MoonIcon.svelte";
+  import ComputerDesktopIcon from "../../components/icons/ComputerDesktopIcon.svelte";
+  import Text from "../../components/typography/Text.svelte";
 
   let isDarkMode: boolean = false;
   let themeLabel: "Toggle Dark Mode" | "Toggle Light Mode";
-  isDarkMode
-    ? (themeLabel = "Toggle Light Mode")
-    : (themeLabel = "Toggle Dark Mode");
 
-  function setTheme() {
+  function setDarkTheme() {
     const root: HTMLElement = document.documentElement;
-    root.classList.toggle("dark");
+    themeLabel = "Toggle Light Mode";
+    root.classList.add("dark");
   }
 
-  function toggleTheme() {
-    isDarkMode = !isDarkMode;
+  function setLightTheme() {
+    const root: HTMLElement = document.documentElement;
+    themeLabel = "Toggle Dark Mode";
+    root.classList.remove("dark");
+  }
+
+  function toggleDarkTheme() {
+    if (isDarkMode === false) {
+      isDarkMode = true;
+      setDarkTheme();
+    }
+  }
+
+  function toggleLightTheme() {
+    if (isDarkMode === true) {
+      isDarkMode = false;
+      setLightTheme();
+    }
+  }
+
+  function togglePreferredTheme() {
+    if (browser) {
+      const root: HTMLElement = document.documentElement;
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        root.classList.add("dark");
+        isDarkMode = true;
+      } else {
+        root.classList.remove("dark");
+        isDarkMode = false;
+      }
+      isDarkMode
+        ? (themeLabel = "Toggle Light Mode")
+        : (themeLabel = "Toggle Dark Mode");
+    }
+  }
+
+  if (browser) {
+    const root: HTMLElement = document.documentElement;
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      root.classList.add("dark");
+      isDarkMode = true;
+    } else {
+      root.classList.remove("dark");
+      isDarkMode = false;
+    }
     isDarkMode
       ? (themeLabel = "Toggle Light Mode")
       : (themeLabel = "Toggle Dark Mode");
-    setTheme();
   }
 
   interface NavItem {
@@ -35,7 +78,7 @@
 
   const navItems: NavItem[] = [
     {
-      name: "All Notes",
+      name: "Notes",
       icon: DocumentDuplicateIcon,
       route: "/notes",
     },
@@ -57,13 +100,13 @@
   ];
 </script>
 
-<div class="app">
+<div class="app text-slate-800 dark:text-slate-200">
   <aside
-    class="w-20 h-full border-r-2 border-slate-300 dark:border-slate-600 flex flex-col justify-between items-center bg-slate-100 dark:bg-slate-800"
+    class="w-20 h-full flex flex-col justify-between items-center gap-4 border-r-2 border-slate-300 dark:border-slate-600 bg-slate-100 dark:bg-slate-800"
   >
     <header class="w-full p-2 flex justify-center items-center">
       <button
-        class="transition p-2 flex justify-center items-center border-2 border-slate-300 hover:border-slate-500 dark:border-slate-700 dark:hover:border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 rounded-lg"
+        class="transition p-2 flex justify-center items-center border-2 border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 rounded-lg"
         title="Open Navigation"
       >
         <Bars2Icon />
@@ -71,24 +114,21 @@
       <div />
     </header>
     <main class="w-full h-full">
-      <div class="w-full px-2 py-6 flex justify-center items-center">
-        <button
-          class="transition p-4 text-sky-500 hover:text-sky-100 border-2 border-sky-300 hover:border-sky-500 dark:border-sky-700 dark:hover:border-sky-500 bg-sky-100 hover:bg-sky-500 dark:bg-sky-900 dark:hover:bg-sky-500 rounded-2xl"
-          title="Create New Note"
-        >
-          <PencilIcon />
-        </button>
-      </div>
       <nav class="w-full p-2">
         <ul class="flex flex-col justify-center items-center gap-2">
           {#each navItems as navItem}
             <li class="flex justify-center items-center">
               <a
                 href={navItem.route}
-                class="transition no-underline p-2 flex justify-center items-center border-2 border-slate-300 hover:border-slate-500 dark:border-slate-700 dark:hover:border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 rounded-lg"
+                class="transition group p-2 flex justify-center items-center border-2 border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 rounded-lg relative"
                 title={navItem.name}
               >
                 <svelte:component this={navItem.icon} />
+                <div
+                  class="transition w-max absolute left-2 group-hover:left-[calc(100%+8px)] hidden group-hover:flex justify-center items-center p-2 rounded-lg border-2 border-slate-300 dark:border-slate-700 bg-slate-100 dark:bg-slate-900"
+                >
+                  <Text as="span">{navItem.name}</Text>
+                </div>
               </a>
             </li>
           {/each}
@@ -98,17 +138,46 @@
       <div />
     </main>
     <footer class="w-full p-2 flex flex-col justify-center items-center gap-4">
-      <button
-        class="transition p-2 flex justify-center items-center border-2 border-slate-300 hover:border-slate-500 dark:border-slate-700 dark:hover:border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 rounded-lg"
-        on:click={toggleTheme}
-        title={themeLabel}
+      <div
+        class="rounded-lg border-2 border-slate-500 flex flex-col justify-center items-center overflow-hidden"
       >
-        {#if isDarkMode}
+        <button
+          class="flex justify-center items-center p-2 border-b-2 border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 relative"
+          on:click={toggleLightTheme}
+          title="Toggle Light Theme"
+        >
           <SunIcon />
-        {:else}
+          <div
+            class="transition duration-300 delay-200 absolute left-[calc(100%+8px)] hidden group-hover:flex justify-center items-center p-2 rounded-lg border-2 border-slate-300 dark:border-slate-700 bg-slate-100 dark:bg-slate-900"
+          >
+            <Text as="span">Light Theme</Text>
+          </div>
+        </button>
+        <button
+          class="flex justify-center items-center p-2 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 relative"
+          on:click={toggleDarkTheme}
+          title="Toggle Dark Theme"
+        >
           <MoonIcon />
-        {/if}
-      </button>
+          <div
+            class="transition duration-300 delay-200 absolute left-[calc(100%+8px)] hidden group-hover:flex justify-center items-center p-2 rounded-lg border-2 border-slate-300 dark:border-slate-700 bg-slate-100 dark:bg-slate-900"
+          >
+            <Text as="span">Dark Theme</Text>
+          </div>
+        </button>
+        <button
+          class="flex justify-center items-center p-2 border-t-2 border-slate-500 bg-slate-100 hover:bg-slate-300 dark:bg-slate-900 dark:hover:bg-slate-700 relative"
+          on:click={togglePreferredTheme}
+          title="Toggle System Default"
+        >
+          <ComputerDesktopIcon />
+          <div
+            class="transition duration-300 delay-200 absolute left-[calc(100%+8px)] hidden group-hover:flex justify-center items-center p-2 rounded-lg border-2 border-slate-300 dark:border-slate-700 bg-slate-100 dark:bg-slate-900"
+          >
+            <Text as="span">System Default</Text>
+          </div>
+        </button>
+      </div>
     </footer>
   </aside>
 </div>
