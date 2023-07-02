dbc1a3f4f1359ed7d83d350dce97174c26dd7f0b
Martin Matovu <matovius@proton.me>
2023-07-02 12:55:03 +0300

    Update variant styles

--- src/components/navigation/Link.svelte
+++ src/components/navigation/Link.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
+  import Text from "../typography/Text.svelte";
+
   export let route: string;
   export let label: string;
+  export let variant:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "error" = "secondary";
+
+  let linkVariant: string;
+
+  if (variant === "primary") {
+    linkVariant = "text-sky-500 hover:text-sky-700 focus:text-sky-700";
+  } else if (variant === "secondary") {
+    linkVariant = "text-slate-500 hover:text-slate-700 focus:text-slate-700";
+  } else if (variant === "success") {
+    linkVariant =
+      "text-emerald-500 hover:text-emerald-700 focus:text-emerald-700";
+  } else if (variant === "warning") {
+    linkVariant = "text-yellow-500 hover:text-yellow-700 focus:text-yellow-700";
+  } else if (variant === "error") {
+    linkVariant = "text-rose-500 hover:text-rose-700 focus:text-rose-700";
+  }
 </script>
 
 <a
   href={route}
-  class="transition font-medium font-body text-base text-sky-500 hover:text-sky-700 dark:hover:text-sky-300 underline underline-offset-2 decoration-2 decoration-sky-200 dark:decoration-sky-800 hover:decoration-sky-500 dark:hover:decoration-sky-500"
+  class={`transition outline-none underline underline-offset-2 decoration-2 ${linkVariant}`}
 >
-  {label}
+  <Text as="span">{label}</Text>
 </a>
