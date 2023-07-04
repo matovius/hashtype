c054550f1908f26e9af6ae1f1a2723d64e7a64b0
Martin Matovu <matovius@proton.me>
2023-07-05 02:14:50 +0300

    Import and customize daisyui default light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,19 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#0ea5e9",
+          secondary: "#6366f1",
+          success: "#10b981",
+          warning: "#eab308",
+          error: "#f43f5e",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
